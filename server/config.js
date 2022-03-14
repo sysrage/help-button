e7829652b827ba6f1ff349ff9baf93c654a56144
Brian Bothwell <sysrage@gmail.com>
2022-03-14 14:27:18 -0400

    add login and cleanup

--- server/config.js
+++ server/config.js
@@ -1,14 +1,8 @@
 const path = require('path');
-const moment = require('moment');
 
 // # Server Configuration
 module.exports = {
   port: process.env.PORT || 4000,
-
-  jwtSecretKey: process.env.JWT_SECRET || 'top_secret',
-
-  helpServerLogDir: path.join(__dirname, 'logs/'), // Log directory
-  helpServerLogFile: `help-button-server-${moment().format('YYYYMMDDhhmmss')}.log`, // Application Log filename
 
   pushoverAppToken: process.env.PO_APP_TOKEN || '',
 
